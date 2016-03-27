c09091a6b710ed5e69fccbdf9f4ad9a424655feb
kingofstorm <wangshuang_jun@163.com>
2016-03-27 21:58:02 +0800

    Update Leetcode.cpp

--- Leetcode.cpp
+++ Leetcode.cpp
@@ -1,3 +1,47 @@
+/*Remove Duplicate from sorted List*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+
+        if(head == NULL)
+            return NULL;
+
+        ListNode * currNode = head;
+        ListNode * nextNode = head->next;
+
+        while(nextNode != NULL)
+        {
+            if(currNode->val == nextNode->val)
+            {
+                ListNode * temp = nextNode;
+                nextNode        = nextNode->next;
+                currNode->next  = nextNode;
+                free(temp);
+
+            }
+            else
+            {
+                nextNode = nextNode->next;
+                currNode = currNode->next;
+            }
+        }
+
+        return head;
+    }
+};
+
+
+
+
 /*258. Add Digits*/
 /*easy*/
 class Solution {
