0fffad54baac46c9adfa9186614937b37640afbb
kingofstorm <wangshuang_jun@163.com>
2016-03-27 17:11:04 +0800

    Update Leetcode.cpp

--- Leetcode.cpp
+++ Leetcode.cpp
@@ -1,1 +1,15 @@
+/* 
+292 Nim Game       Easy 
 
+*/
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n<=3)
+            return true;
+        if(n%4==0)
+            return false;
+        else
+            return true;
+    }
+};
