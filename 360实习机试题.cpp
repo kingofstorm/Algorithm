6272da30446deebe3dbe2679552cd89a1b9b300f
kingofstorm <wangshuang_jun@163.com>
2016-03-28 22:06:13 +0800

    Update 360实习机试题.cpp

--- 360实习机试题.cpp
+++ 360实习机试题.cpp
@@ -73,3 +73,77 @@ int main()
 	}
 	return 0;
 }
+
+/*第二题*/
+
+
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cstdio>
+#include<cstdlib>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main()
+{
+    int n;
+	int b;
+	while (cin>>n>>b)
+	{
+	
+		if (n <= 0 || n > 3000000)//记不清最大多少了
+			break;
+		if (n == 1)
+		{
+			cout << 1 << endl;
+			continue;
+		}
+		if (n % 2 == 0)
+			{	
+				int Premiddle = n / 2 + 1;
+				int Postmiddle = n / 2;
+				if (b >= Premiddle)
+				{
+					int a = b-1;
+					cout << a << endl;
+					continue;
+				}
+				if (b<=Postmiddle)
+				{
+					int a = b+1;
+					cout << a << endl;
+					continue;
+				}
+			}
+		if (n % 2 == 1)
+			{
+				int middle =(n + 1) / 2;
+				if (b == middle)
+				{
+					int a = n / 2;
+					cout << a << endl;
+					continue;
+				}
+				if (b > middle)
+				{
+					int a = b-1;
+					cout << a << endl;
+					continue;
+				}
+
+				if (b<middle)
+				{
+					int a = b+1;
+					cout << a << endl;
+					continue;
+				}
+			}
+
+	}
+	
+	return 0;
+}
+
