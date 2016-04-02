4cb2128de4fabc4395a4e9c1ec9a164cbf9f4c74
kingofstorm <wangshuang_jun@163.com>
2016-04-02 12:39:53 +0800

    Update Leetcode.cpp

--- Leetcode.cpp
+++ Leetcode.cpp
@@ -8,6 +8,38 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+ /*O(1)*/
+ 
+ class Solution {
+public:
+   bool isPalindrome(ListNode* head) {
+      if( head==NULL || head->next==NULL ) return true;
+      // list has at least two nodes
+      ListNode *pre = NULL, *pfirst, *psecond;
+      auto px1 = head, px2 = head;
+      // traversal 1: reverse the first half and get pfirst&psecond in position
+      while(true) {
+         px2 = px2->next->next;        // move px2 forward
+         pfirst = px1; px1 = px1->next;// move px1 forward
+         pfirst->next = pre;           // point curent next back
+         if( px2 == NULL ) {           // 2n Nodes
+            psecond = px1; break;
+         } else if( px2->next == NULL ) { // 2n+1 Nodes
+            psecond = px1->next; break;
+         }
+         pre = pfirst;
+      }
+      // traversal 2: check node values
+      while( pfirst!=NULL ) {
+         if( pfirst->val != psecond->val ) return false;
+         pfirst = pfirst->next; psecond = psecond->next;
+      }
+      return true;
+   }
+};
+
+
+
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
