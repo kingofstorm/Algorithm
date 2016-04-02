b14679f2c2e22467983875da1e7e626361191f76
kingofstorm <wangshuang_jun@163.com>
2016-04-02 13:11:53 +0800

    Update Leetcode.cpp

--- Leetcode.cpp
+++ Leetcode.cpp
@@ -1,3 +1,37 @@
+/*
+字符串匹配
+KMP算法
+
+
+*/
+
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int len1=haystack.size();
+        int len2=needle.size();
+        if(len1==0&&len2==0)
+            return 0;
+        if(len1<len2)
+            return -1;
+        int i,j;
+        for( i=0;i<=len1-len2;i++)
+            {
+                for( j=0;j<len2;j++)
+                {
+                    if(haystack[i+j]!=needle[j])
+                    break;
+                    
+                }
+                if(j==len2)
+                return i;
+            }
+        return -1;
+    }
+};
+
+
 /*205. Isomorphic Strings*/
 class Solution {  
 public:  
