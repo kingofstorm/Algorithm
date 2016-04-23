deb6ccc9de3037ed404e55b26ee68ad6d70af60c
kingofstorm <wangshuang_jun@163.com>
2016-04-23 21:30:48 +0800

    Update cplusplus.cpp

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,3 +1,45 @@
+/*单继承*/
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Triangle{
+public:
+	void triangle(){
+		cout<<"I am a triangle\n";
+	}
+};
+
+class Isosceles : public Triangle{
+public:
+	void isosceles(){
+		cout<<"I am an isosceles triangle\n";
+	}
+	void triangle(){
+		cout<<"I am a triangle\n";
+	}
+};
+class  Equilateral:public Isosceles{
+public:
+	void  equilateral()
+	{
+		cout<<"I am an equilateral triangle"<<endl;
+	}
+};
+
+//Write your code here.
+int main(){
+
+	Equilateral eqr;
+	eqr.equilateral();
+	eqr.isosceles();
+	eqr.triangle();
+	system("pause");
+	return 0;
+}
 
 
 /*多重继承*/
